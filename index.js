62c417de758a939bd7bc894136738837f0939333
Ciobanu Dumitru <ciobanu.dumitru204@gmail.com>
2024-02-13 17:26:23 +0000

    changed app port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = 80;
 const cors = require('cors');
 const session = require('express-session');
 const bodyParser = require('body-parser');
